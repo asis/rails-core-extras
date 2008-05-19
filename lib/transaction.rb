2d316fd85f6df264cbccc0a88a90ad16399b3ec5
Asís García <asis.garcia@trabesoluciones.com>
2008-05-20 00:12:36 +0200

    Minor fixes

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -4,32 +4,36 @@ module RailsCoreExtras
   #
   module TransactionExtensions
     #
-    # Exception raised when there is no transaction active in the current 
+    # Exception raised when there is no open transaction in the current 
     # context
     #
     class WithinTransactionError < Exception
       def initialize
-	super("No open transactions in the current thread")
+      	super("No open transactions in the current thread")
       end
     end
     
     module Methods
       #
-      # Raise a #WithinTransactionError when there is no transaction open in the
+      # Raise a #WithinTransactionError when there is no open transaction in the
       # current thread context
       #
       def assert_within_transaction
-	raise WithinTransactionError.new unless within_transaction?
+	      raise WithinTransactionError.new unless within_transaction?
       end
       
       #
       # Returns +true+ when called within a transaction, +false+ otherwise
       #
       def within_transaction?
-	!!Thread.current['open_transactions']
+	      !Thread.current['open_transactions'].nil? && 
+	        Thread.current['open_transactions'] > 0
       end
     end
     
+    #
+    # Make methods available as instance and class methods
+    #
     def self.included(base)
       base.send(:include, Methods)
       base.extend(Methods)
